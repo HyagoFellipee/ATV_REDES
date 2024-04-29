97755bfe312c1669022a2bb0d8a0e3f47503c669
Hyago <hyago.soares@cear.ufpb.br>
2024-04-29 18:34:34 -0300

    cria as classes principais da atividade

--- udp/udp_client.py
+++ udp/udp_client.py
@@ -1,58 +1,114 @@
-import socket, random, os
-from dotenv import load_dotenv
+import socket, random
 from udp_message import UdpMessage 
 
+LOCALHOST = "127.0.0.1"
+PORTA_CLIENTE = 8000
+PORTA_SERVIDOR = 50000
+
+class UdpClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
 
-def send_udp_message(message, host, port):
+    def start_client(self, host_server, port_server):
+        
+        while True:
+        # Menu de opções
 
-    # Cria um socket UDP
-    client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+            print("\nSelecione o tipo de request que deseja fazer: ")
+            print("0 - Data e Hora")
+            print("1 - Mensagem Motivacional")
+            print("2 - Quantidade de repostas emitidas até o momento")
+            print("3 - Encerrar cliente")
 
-    # Tenta enviar a mensagem
-    try:
-        client_socket.sendto(message, (host, port))
-        print("\nMensagem enviada com sucesso")
-    except Exception as e:
-        print("\nErro ao enviar mensagem: ", e)
-    finally:
-        client_socket.close()
+            request_type = int(input("Digite o número correspondente ao tipo de request: "))
+
+
+            if request_type == 3:
+                print("Cliente encerrado")
+                break
+
+            # O identificador é um número aleatório de 1 a 65535 que mudará a cada request
+            identifier = random.randint(1, 65535)
+
+
+            # Cria a mensagem
+            message = UdpMessage(1, request_type, identifier)
+
+            print("\nMensagem criada: ", message)
+
+            # Envia a mensagem
+            self.send_udp_message(message.as_bytes(), host_server, port_server)
+
+            print("Aguardando resposta...")
+
+            print("Servidor criado para receber resposta: ", self.host, self.port)
+
+            self.receive_udp_message(self.host, self.port)
+
+            # Recebe a resposta
+
+    def receive_udp_message(self, host, port):
+
+        client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+        client_socket.bind((host, port))
+
+        try: 
+
+            message, _ = client_socket.recvfrom(4096)
+
+            received_response = UdpMessage.from_bytes(message)
+
+
+            received_is_request = int(received_response.is_request, 2)
+
+            if received_is_request:
+                raise Exception("Mensagem não é uma resposta")
+            
+            received_request_type = int(received_response.request_type, 2)
+
+            if received_request_type == 3:
+                raise Exception("Request foi julgado inválido pelo servidor")
+
+            
+
+            message = received_response.message.decode()
+
+            print("\nResposta recebida: ", message)
+
+        except Exception as e:
+            print("\nErro ao receber resposta: ", e)
+            
+
+    def send_udp_message(self, message, host, port):
+
+        # Cria um socket UDP
+        self.client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.client_socket.bind((self.host, self.port))
+
+        # Tenta enviar a mensagem
+        try:
+            self.client_socket.sendto(message, (host, port))
+            print("\nMensagem enviada com sucesso")
+        except Exception as e:
+            print("\nErro ao enviar mensagem: ", e)
+        finally:
+            self.client_socket.close()
 
 
 
     
 if __name__ == "__main__":
 
+    client = UdpClient(LOCALHOST, PORTA_CLIENTE)
+    client.start_client(LOCALHOST, PORTA_SERVIDOR)
 
 
-    # Iniciando o cliente
-    load_dotenv()
-    LOCALHOST = os.getenv("LOCALHOST")
-    PORTA_SERVIDOR = int(os.getenv("PORTA_SERVIDOR"))
+ 
 
-    while True:
-        # Menu de opções
-
-        print("\nSelecione o tipo de request que deseja fazer: ")
-        print("1 - Data e Hora")
-        print("2 - Mensagem Motivacional")
-        print("3 - Quantidade de repostas emitidas até o momento")
-        print("4 - Encerrar servidor")
-
-        request_type = int(input("Digite o número correspondente ao tipo de request: "))
-
-        if request_type == 4:
-            print("Cliente encerrado")
-            break
-
-        # O identificador é um número aleatório de 1 a 65535 que mudará a cada request
-        identifier = random.randint(1, 65535)
-
-        # Cria a mensagem
-        message = UdpMessage(0, request_type, identifier)
-
-        # Envia a mensagem
-        send_udp_message(message.as_bytes(), LOCALHOST, PORTA_SERVIDOR)
+   
 
 
 
--- udp/udp_message.py
+++ udp/udp_message.py
@@ -1,4 +1,3 @@
-
 class UdpMessage: 
     def __init__(self, is_request, request_type, identifier, message_length=0, message=""):
         
@@ -8,12 +7,14 @@ class UdpMessage:
         self.message_length = bin(message_length)[2:].zfill(8) # 8 bits
         self.message        = message.encode() # depende de message_length
 
-        self.header_bytes_list = [self.is_request, self.request_type, self.identifier[:8], self.identifier[8:], self.message_length]
+        self.header_bytes_list = [self.is_request + self.request_type, self.identifier[:8], self.identifier[8:]]
 
-        self.bytes_list = [self.is_request+self.request_type, self.identifier[:8], self.identifier[8:], self.message_length]
+        self.bytes_list = self.header_bytes_list.copy()
 
-        for byte in self.message:
-            self.bytes_list.append(bin(byte)[2:].zfill(8))
+        if message_length:
+            self.bytes_list.append(self.message_length)
+            for byte in self.message:
+                self.bytes_list.append(bin(byte)[2:].zfill(8))
 
         self.whole_binary = "".join(self.bytes_list)
 
@@ -22,7 +23,22 @@ class UdpMessage:
 
         
     def as_bytes(self):
+
         return bytes([int(byte, 2) for byte in self.bytes_list])
+    
+    def from_bytes(bytes_message):
+
+        binary_message = ''.join(format(byte, '08b') for byte in bytes_message)
+
+        is_request = int(binary_message[:4], 2)
+        request_type = int(binary_message[4:8], 2)
+        identifier = int(binary_message[8:24], 2)
+        message_length = int(binary_message[24:32], 2) if len(binary_message) > 32 else 0
+
+        message = bytes_message[4:].decode() if message_length else ""
+
+
+        return UdpMessage(is_request, request_type, identifier, message_length, message)
 
     def __str__(self):
 
