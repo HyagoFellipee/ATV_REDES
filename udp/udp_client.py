7bfba2b2b04163e473e02229074f92890184181a
Hyago <hyago.soares@cear.ufpb.br>
2024-05-01 10:16:09 -0300

    refatoração do udp message e client

--- udp/udp_client.py
+++ udp/udp_client.py
@@ -3,15 +3,26 @@ from udp_message import UdpMessage
 
 class UdpClient:
     def __init__(self, host, port):
+        '''
+        Inicializa o cliente UDP
+        host: IP do cliente
+        port: Porta do cliente
+        '''
+
         self.host = host
         self.port = port
 
 
     def start_client(self, host_server, port_server):
-        
-        while True:
-        # Menu de opções
+        ''' 
+        Inicia o cliente UDP 
+        host_server: IP do servidor
+        port_server: Porta do servidor
+        '''
 
+        while True:
+            
+            # Menu de opções
             print("\nSelecione o tipo de request que deseja fazer: ")
             print("0 - Data e Hora")
             print("1 - Mensagem Motivacional")
@@ -28,65 +39,77 @@ class UdpClient:
             # O identificador é um número aleatório de 1 a 65535 que mudará a cada request
             identifier = random.randint(1, 65535)
 
-
             # Cria a mensagem
             message = UdpMessage(0, request_type, identifier)
 
             print("\nMensagem criada: ", message)
 
-            # Envia a mensagem
+            # Envia a mensagem como bytes
             self.send_udp_message(message.as_bytes(), host_server, port_server)
 
             print("Aguardando resposta...")
 
-
+            # Recebe a resposta
             self.receive_udp_message()
 
-            # Recebe a resposta
 
     def receive_udp_message(self):
+        '''
+        Inicia o servidor UDP para receber a resposta do servidor
+        '''
+
+        # Cria o socket do tipo UDP
+        # AF_INET indica que é um endereço IP
+        # SOCK_DGRAM indica que é um socket do tipo UDP
 
         client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+
+        # Associa o socket ao endereço e porta do cliente
         client_socket.bind((self.host, self.port))
-
-        try: 
             
-            print("Servidor criado para receber resposta: ", self.host, self.port)
-            message, _ = client_socket.recvfrom(4096)
+        # Cria um buffer para receber a mensagem
+        message, _ = client_socket.recvfrom(4096)
+
+        # Converte a mensagem recebida em um objeto UdpMessage
+        received_response = UdpMessage.from_bytes(message)
 
 
-            received_response = UdpMessage.from_bytes(message)
+        received_is_response = int(received_response.is_response)
 
+        if not received_is_response:
+            raise Exception("Mensagem não é uma resposta")
+        
+        received_request_type = int(received_response.request_type)
 
-            received_is_response = int(received_response.is_response, 2)
+        if received_request_type == 3:
+            raise Exception("Request foi julgado inválido pelo servidor")
 
-            if not received_is_response:
-                raise Exception("Mensagem não é uma resposta")
-            
-            received_request_type = int(received_response.request_type, 2)
+        
 
-            if received_request_type == 3:
-                raise Exception("Request foi julgado inválido pelo servidor")
+        message = received_response.message.decode()
 
-            
-    
-            message = received_response.message.decode()
+        print("\nResposta recebida: ", message)
 
-            print("\nResposta recebida: ", message)
-
-        except Exception as e:
-            print("\nErro ao receber resposta: ", e)
-            
 
     def send_udp_message(self, message, host, port):
-     
-        # Create a UDP socket
-        client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        '''
+        Envia uma mensagem para o servidor
+        message: Mensagem a ser enviada
+        host: IP do servidor
+        port: Porta do servidor
+        '''
 
+        # Cria o socket do tipo UDP
+        # AF_INET indica que é um endereço IP
+        # SOCK_DGRAM indica que é um socket do tipo UDP
+
+        client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        
+        # Associa o socket ao endereço e porta do cliente, necesário para receber a resposta
         client_socket.bind((self.host, self.port))
         
-        # Attempt to send the message
+        # Envia a mensagem para o servidor
         client_socket.sendto(message, (host, port))
         client_socket.close()
     
--- udp/udp_message.py
+++ udp/udp_message.py
@@ -1,36 +1,45 @@
+import struct
+
+
 class UdpMessage: 
     def __init__(self, is_response, request_type, identifier, message_length=0, message=""):
         
-        self.is_response     = bin(is_response)[2:].zfill(4) # 4 bits
-        self.request_type   = bin(request_type)[2:].zfill(4) # 4 bits 
-        self.identifier     = bin(identifier)[2:].zfill(16) # 16 bits
-        self.message_length = bin(message_length)[2:].zfill(8) # 8 bits
-        self.message        = message.encode() # depende de message_length
-
-        self.header_bytes_list = [self.is_response + self.request_type, self.identifier[:8], self.identifier[8:]]
-
-        self.bytes_list = self.header_bytes_list.copy()
-
-        if message_length:
-            self.bytes_list.append(self.message_length)
-            for byte in self.message:
-                self.bytes_list.append(bin(byte)[2:].zfill(8))
+        self.is_response    = is_response       # 4 bits
+        self.request_type   = request_type      # 4 bits 
+        self.identifier     = identifier        # 16 bits
+        self.message_length = message_length    # 8 bits
+        self.message        = message.encode()  # depende do message_length
 
         
     def as_bytes(self):
 
-        return bytes([int(byte, 2) for byte in self.bytes_list])
+        # struct.pack converte os valores em bytes dado um formato e uma lista de valores
+        # ! indica que a ordem dos bytes é big-endian, que é o formato de rede mais comum
+        # B indica que é um unsigned char (1 byte)
+        # H indica que é um unsigned short (2 bytes)
+
+        is_response_and_request_type = (self.is_response << 4) + self.request_type
+
+        header = struct.pack('!BHB', is_response_and_request_type, self.identifier, self.message_length)
+        
+        return header + self.message
     
     def from_bytes(bytes_message):
 
-        binary_message = ''.join(format(byte, '08b') for byte in bytes_message)
+        # struct.unpack converte os bytes em valores dado um formato e uma lista de bytes
+        # ! indica que a ordem dos bytes é big-endian, que é o formato de rede mais comum
+        # B indica que é um unsigned char (1 byte)
+        # H indica que é um unsigned short (2 bytes)
 
-        is_response = int(binary_message[:4], 2)
-        request_type = int(binary_message[4:8], 2)
-        identifier = int(binary_message[8:24], 2)
-        message_length = int(binary_message[24:32], 2) if len(binary_message) > 32 else 0
+        is_response_and_request_type, identifier, message_length = struct.unpack('!BHB', bytes_message[:4])
+
+        is_response = is_response_and_request_type >> 4 # shift para pegar os 4 bits mais significativos
+        request_type = is_response_and_request_type & 0b00001111 # mascara para pegar os 4 bits menos significativos
 
 
+        # Se a mensagem existe, verifica se é um número ou uma string pela request_type
+        # Se for um número, converte para string
+        # Se for uma string, decodifica para UTF-8
 
         if message_length:
             if request_type == 2:
@@ -45,12 +54,13 @@ class UdpMessage:
 
     def __str__(self):
 
+        # Formata a mensagem para exibição, apenas para facilitar a visualização
 
         message = ""
 
         # Header como hexadecimal
-        for byte in self.header_bytes_list:
-            message += str(hex(int(byte, 2))) + " "
+        for byte in self.as_bytes()[0:4]:
+            message += str(hex(byte)) + " "
 
         # Mensagem como caracteres
         for byte in self.message:
